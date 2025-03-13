aec2233ce9b9a22605fd132de0f0ba67c64f7ece
AbhiRamMaddali <abhiram.maddali1204@gmail.com>
2025-03-13 23:22:46 +0530

    Update Binary search.py

--- Binary search.py
+++ Binary search.py
@@ -1,19 +1,21 @@
 def binary(arr,x):
   st=0
   l=len(arr)
+  mid=0
   while(st<=l):
-    mid=(st+l)/2
-    if(x>arr[mid]):
-      st=mid+1
-    elif (x<arr[mid]):
-      l=mid-1
-    else:
-      return mid
-    return -1
-arr = [ 2, 3, 4, 10, 40 ]
-x = 10
+      mid=(st+l)//2
+      if(x>arr[mid]):
+          st=mid+1
+      elif(x<arr[mid]):
+          l=mid-1
+      else:
+          return mid
+  return -1
+arr=[2,3,4,10,5]
+x=10
 result=binary(arr,x)
 if result !=-1:
-  print(result)
+    print(result)
 else:
-  print('not found')
+    print('not found')
+  // check indent
